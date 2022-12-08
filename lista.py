643e9aaaae01dcdee8f736a4918a307671ebed39
rotivo <vitorfernando@usp.br>
2022-12-08 07:42:42 -0300

    lista.py

---
+++ lista.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#Criando uma lista no python3
+lista_a = ["Tinky_Winky", "Dipsy", "Lala", "Poo", "Teletubbies"]
+
+print(lista_a[0])
+print(lista_a[1])
+print(lista_a[2])
+print(lista_a[3])
+print(lista_a[4])
+
+#Trocando um item da lista por outro e vendo como a lista fica
+lista_a[0] = "Bartolomeu"
+print(lista_a[0])
+print(lista_a)
